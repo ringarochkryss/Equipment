ed4281a7db5923029550228e791e566178166c6e
ringarochkryss <petra@mellbrand.com>
2022-07-19 22:13:33 +0200

    Add project files.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Equipment.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+        public ActionResult Random()
+        {
+            return View();
+        }
+    }
+}
